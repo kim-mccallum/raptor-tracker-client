08573407998f56d485112158fbf09146a1e3c8fc
Kim McCallum <kchaase@gmail.com>
2020-04-19 07:17:19 -0600

    created selection menu overlay

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,24 +1,45 @@
-import React from 'react'
-import SelectionMenu from './SelectionMenu';
+import React, { Component } from 'react'
+import SelectionMenu from './SelectionMenu'
 import './Nav.css'
 
-export default function Nav() {
-    return (
-        <>
-            <nav className='app-nav'>
-                <img className="logo" src="https://hawkwatch.org/images/HWI-logo_tag2.png" alt="HWI logo"/>
-                <ul>
-                    <li><a href="#Eagles" className="active">Golden Eagles</a></li>
-                    <li><a href="#services">African Vultures</a></li>
-                    <li><a href="#clients">Learn about tracking research</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-                <div className="selection-menu">
-                    <SelectionMenu />
-                </div>
-            </nav>
+export default class Nav extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             isMenuVisible: true
+        }
+    }
 
-        </>
-    )
+    handleToggleMenu = (e) => {
+        e.preventDefault();
+        this.setState({
+            isMenuVisible: !this.state.isMenuVisible
+        })
+    }
+    
+    render() {
+        let menu;
+        if(this.state.isMenuVisible === true){
+            menu = <SelectionMenu onClick={this.handleToggleMenu}/>
+        }
+        return (
+            <>
+                <nav className='app-nav'>
+                    <img className="logo" src="https://hawkwatch.org/images/HWI-logo_tag2.png" alt="HWI logo"/>
+                    <ul>
+                        <li><a href="#Eagles" className="active" onClick={this.handleToggleMenu}>Golden Eagles</a></li>
+                        <li><a href="#Vultures" onClick={this.handleToggleMenu}>African Vultures</a></li>
+                        <li><a href="https://hawkwatch.org/our-work/tracking" target="blank">Learn about tracking research</a></li>
+                        <li><a href="https://hawkwatch.org/about/contact-us" target="blank">Contact</a></li>
+                    </ul>
+                </nav>
+                <>
+                    {menu}
+                </>
+            </>
+        )
+    }
 }
+
 
--- src/Components/SelectionMenu.js
+++ src/Components/SelectionMenu.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './SelectionMenu.css'
 
-export default function SelectionMenu() {
+export default function SelectionMenu(props) {
     return (
-        <div className="banner-overlay">
-            <h2>Menu overlay on map coming soon</h2>
+        <div className="selection-menu">
+            <div className="banner-overlay">
+                <button 
+                    type="button" 
+                    className="close-btn" 
+                    aria-label="Close"
+                    onClick={props.onClick}
+                >&times;</button>
+                <h2>Menu overlay on map coming soon</h2>
+            </div>
         </div>
     )
 }
