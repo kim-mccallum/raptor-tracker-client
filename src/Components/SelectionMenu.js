2c8d4cd423e446e1b19f2e78be523dd769ff2af2
Kim McCallum <kchaase@gmail.com>
2020-04-19 19:02:54 -0600

    basic html in place

--- src/Components/SelectionMenu.js
+++ src/Components/SelectionMenu.js
@@ -1,18 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './SelectionMenu.css'
+import StudyInformation from './StudyInformation'
 
-export default function SelectionMenu(props) {
-    return (
-        <div className="selection-menu">
-            <div className="banner-overlay">
-                <button 
-                    type="button" 
-                    className="close-btn" 
-                    aria-label="Close"
-                    onClick={props.onClick}
-                >&times;</button>
-                <h2>Menu overlay on map coming soon</h2>
-            </div>
-        </div>
-    )
+export default class SelectionMenu extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             individuals: [],
+             startDate: {
+                 value: '',
+                 touched: false
+             },
+             endDate: {
+                value: '',
+                touched: false
+            }
+        }
+    }
+    // Add handlers to get the data. 
+    // The selected data will need to be passed back to App and to Map to render on the map
+    
+    // Get a list of individuals in the eagle dataset
+    render() {
+        return (
+                <div className="selection-menu">
+                    <div className="banner-overlay">
+                        <button 
+                            type="button" 
+                            className="close-btn" 
+                            aria-label="Close"
+                            onClick={this.props.onClick}
+                        >&times;</button>
+                        <form className="selectDataForm">
+                            <fieldset>
+                                <legend>Select viewing parameters</legend>
+                                <label htmlFor="start-date">Start Date:</label>
+                                <input type="date" id="start-date" name="start-date" />
+                                <label htmlFor="end-date">End Date:</label>
+                                <input type="date" id="end-date" name="end-date" />
+                                <label htmlFor="fname">Select Individuals:</label>
+                                <select id="eagles" name="eagles">
+                                    {/* Get this from a list of individuals in the data */}
+                                    <option value="Glenn">Glenn</option>
+                                    <option value="Lewis">Lewis </option>
+                                    <option value="Frey">Frey</option>
+                                    <option value="Benedict">Benedict Cumberpatch</option>
+                                </select>
+                                <button type="submit" value="Submit">Map the data!</button>
+                            </fieldset>
+                        </form>
+                        <StudyInformation />
+                    </div>
+                </div>
+            )
+    }
 }
+
---
+++ src/Components/StudyInformation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function StudyInformation() {
+    return (
+        <div>
+            <h1>Study Information Specific to the data in state:</h1>
+            <p>Lots of cool information about the study... how many birds, since when, etc. and maybe links to more info from HWI. 'Deserunt sit laborum duis non Lorem nulla anim velit sunt velit duis sit ex. Commodo ex sit mollit officia mollit fugiat id occaecat nisi dolor.'</p>
+            <h3>COOL PICTURE FROM THE STUDY</h3>
+        </div>
+    )
+}
